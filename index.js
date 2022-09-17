ee3b5c89c46a0360b171567969018e56b1593651
Andrew Wachira <andrewwachira1@gmail.com>
2022-09-17 20:12:07 +0300

    returning a response

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 app.get("/ipn",(req,res)=>{
     console.log(req.body)
+     return  res.json({"message":"Data received" })
 })
 app.listen(process.env.PORT || 3000)
