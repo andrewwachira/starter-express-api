ffb8a26114274441b9a82e10bd97abc1038af135
Andrew <andrewwachira1@gmail.com>
2023-07-23 07:14:46 +0300

    just testing kopokopo api

--- controllers/mpesa.js
+++ controllers/mpesa.js
@@ -4,20 +4,21 @@ const StrongKombuchaOrder  = require("../models/StrongKombuchaOrder");
 
 const makeMpesaPayments = async (req,res)=>{
     try{
-        const {Body:{stkCallback:{MerchantRequestID,CheckoutRequestID,ResultCode,ResultDesc}}} = req.body;
-        if(MerchantRequestID){
-            const order = await Order.findOne({mpesaIdentifier:CheckoutRequestID})
-            const cateringOrder = await CateringOrder.findOne({mpesaIdentifier:CheckoutRequestID})
-            const strongKombuchaOrder = await StrongKombuchaOrder.findOne({mpesaIdentifier:CheckoutRequestID})
-            const theOrder = [order,cateringOrder,strongKombuchaOrder].find(theOne => theOne !== null && theOne !== undefined); 
+        console.log(req.body)
+        // const {Body:{stkCallback:{MerchantRequestID,CheckoutRequestID,ResultCode,ResultDesc}}} = req.body;
+        // if(MerchantRequestID){
+        //     const order = await Order.findOne({mpesaIdentifier:CheckoutRequestID})
+        //     const cateringOrder = await CateringOrder.findOne({mpesaIdentifier:CheckoutRequestID})
+        //     const strongKombuchaOrder = await StrongKombuchaOrder.findOne({mpesaIdentifier:CheckoutRequestID})
+        //     const theOrder = [order,cateringOrder,strongKombuchaOrder].find(theOne => theOne !== null && theOne !== undefined); 
 
-            console.log("*************Order from DB**************")
-            if(theOrder.mpesaIdentifier === CheckoutRequestID){
-                const update = {MerchantRequestID,CheckoutRequestID,ResultCode,ResultDesc }
-                console.log("**************Payload from Safaricom**************")
-                await theOrder.updateOne({mpesaPaymentResult:update})
-            }
-        }
+        //     console.log("*************Order from DB**************")
+        //     if(theOrder.mpesaIdentifier === CheckoutRequestID){
+        //         const update = {MerchantRequestID,CheckoutRequestID,ResultCode,ResultDesc }
+        //         console.log("**************Payload from Safaricom**************")
+        //         await theOrder.updateOne({mpesaPaymentResult:update})
+        //     }
+        // }
         let message={
           "ResponseCode": "00000000",
           "ResponseDesc": "success"
